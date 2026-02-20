dc31e432dca7031e2db1622ec0816061f863bbfe
Jayakumar S <jayakumarselvaraj01@gmail.com>
2026-02-20 16:48:05 +0530

    Updated portfolio changes

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -29,11 +29,14 @@ function Navbar() {
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
           </li>
-          <li>
+          {/* <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link>
-          </li>
+          </li> */}
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+          </li>
+          <li>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#contact"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">CONTACT</div></Link>
           </li>
         </ul>
       </div>
